5a8f84fdad967652e2a95e54ee04541c240ee02a
unknown <joyce.mungai@simpleformations.ke>
2023-07-17 21:49:53 +0300

    Install react router and user components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
 import HomeScreen from './components/HomeScreen/HomeScreen';
+import {  Routes, Route } from 'react-router-dom';
+import About from './components/About/About';
+import Users from './components/Users/Users';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="app">
       <HomeScreen />
+      <div>
+        <Routes>
+          <Route path="/about" element={About} />
+          <Route path="/users" element={Users} />
+          <Route path="/" element={Home} />
+        </Routes>
+      </div>
     </div>
   );
 }

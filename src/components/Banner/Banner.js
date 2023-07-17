cfebd8fd58e10e6b630042e97d5bd1fd9ee0f1cf
unknown <joyce.mungai@simpleformations.ke>
2023-07-17 08:54:29 +0300

    Amend the banner component

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import './Banner.css'
+import axios from '../../axios'
+import requests from '../../Requests'
 
 function Banner() {
+  const [movie, setMovie] = useState([])
+
+  useEffect(()=>{
+    async function fetchData(){
+      const request = await axios.get(requests.fetchNetflixOriginals)
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length -1)
+        ]
+      )
+      return request
+    }
+  },[])
 
   function truncate(string, n){
     return string?.length > n ? string.substr(0,n-1) + '...' : string;
@@ -15,14 +30,16 @@ function Banner() {
       backgroundPosition:"center center",
     }}>
       <div className="banner__contents">
-        <h1 className="banner__title">Movie Name</h1>
+        <h1 className="banner__title">
+          Movie Name
+        </h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My list</button>
         </div>
         <h1 className="banner__description">
           {truncate(
-            `This is a test description This is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test description`, 150)}</h1>
+            `This is a test description This is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test description`,150)}</h1>
       </div>
       <div className="banner--fadeBottom" />
 
